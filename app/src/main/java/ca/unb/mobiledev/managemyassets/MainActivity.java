a5b3193f782ecc8faea0000246a3dc6bd8cb3e06
ShutUpChips <laverjoshua@gmail.com>
2018-02-17 12:05:53 -0400

    4 Added Map Activity 4 Changed Manifest with new activity 4 Created Map Layout 4 Create temp button to switch between activities

--- app/src/main/java/ca/unb/mobiledev/managemyassets/MainActivity.java
+++ app/src/main/java/ca/unb/mobiledev/managemyassets/MainActivity.java
@@ -1,7 +1,10 @@
 package ca.unb.mobiledev.managemyassets;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button button = (Button) findViewById(R.id.button_send);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent mapIntent = new Intent(MainActivity.this, MapViewActivityFragment.class);
+                startActivity(mapIntent);
+            }
+        });
     }
+
+
+
+
 }
