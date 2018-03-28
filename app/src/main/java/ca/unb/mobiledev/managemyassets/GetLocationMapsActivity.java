df970e9d54e59674e0e80d53958880bdf1cdcac2
Jason Cleveland <jasonncleveland@gmail.com>
2018-03-28 18:02:55 -0300

    Changed get location by map click to center on device location

--- app/src/main/java/ca/unb/mobiledev/managemyassets/GetLocationMapsActivity.java
+++ app/src/main/java/ca/unb/mobiledev/managemyassets/GetLocationMapsActivity.java
@@ -1,9 +1,18 @@
 package ca.unb.mobiledev.managemyassets;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -11,6 +20,8 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class GetLocationMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private LocationManager mLocationManager;
+    private LocationListener mLocationListener;
     private GoogleMap mMap;
     private Asset asset;
 
@@ -23,16 +34,47 @@ public class GetLocationMapsActivity extends AppCompatActivity implements OnMapR
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
         if (getIntent().getExtras() != null)
             asset = (Asset) getIntent().getExtras().get(Asset.OBJECT_NAME);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MapActivity.PERMISSION_REQUEST_ACCESS_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationListener();
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -55,5 +97,24 @@ public class GetLocationMapsActivity extends AppCompatActivity implements OnMapR
                 startActivity(intent);
             }
         });
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            startLocationListener();
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MapActivity.PERMISSION_REQUEST_ACCESS_FINE_LOCATION);
+            }
+        }
+    }
+
+    private void startLocationListener() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            String provider = mLocationManager.getBestProvider(new Criteria(), true);
+            Location location = mLocationManager.getLastKnownLocation(provider);
+            if (location != null) {
+                mLocationListener.onLocationChanged(location);
+            }
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, mLocationListener);
+        }
     }
 }
